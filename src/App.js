ec099c62c55d36dbe332d7f3c20e1d972451a14d
Abdullah Celen <abdullahcelen009@gmail.com>
2020-06-19 13:14:12 +0100

    4
    4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Scoreboard from "./ScoreBoard";
+import countryScores from "./countryScores";
 
 const App = () => {
+  const [scoreOrder, setScoreOrder] = useState("Sort Ascending");
+  const [country, setCountry] = useState(countryScores);
+
+  const handleButtonClick = () => {
+    if (scoreOrder === "Sort Ascending") {
+      countryScores.map((a) => a.scores.sort((a, b) => a.s - b.s));
+      setScoreOrder("Sort Descending");
+    } else {
+      countryScores.map((a) => a.scores.sort((a, b) => b.s - a.s));
+      setScoreOrder("Sort Ascending");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const findCountry = e.target.value.toLowerCase();
+    setCountry(
+      countryScores.filter((item) =>
+        item.name.toLowerCase().includes(findCountry)
+      )
+    );
+  };
   return (
     <div className="App">
       <h1>High Scores Per Country </h1>
+      <button name="button" className="button" onClick={handleButtonClick}>
+        {" "}
+        {scoreOrder}
+      </button>
+      <input
+        type="text"
+        className="input"
+        onChange={handleInputChange}
+        placeholder="find country"
+      />
       <Scoreboard />
     </div>
   );
